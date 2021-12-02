076775a775813c18cd16f8197ca6217c129f30ba
Jonathan Marek <jon.m.marek@gmail.com>
2021-12-02 16:50:50 -0600

    Finish day 1

--- src/day-1-part-2.py
+++ src/day-1-part-2.py
@@ -3,12 +3,17 @@ import os
 with open(os.path.join('input', 'day-1-input.txt'), 'r') as f:
     vals = [int(x.strip('\n')) for x in f.readlines()]
     
-current_depth = vals.pop(0)
+window_depths = []
+
+for i in range(0, len(vals) - 2):
+    window_depths.append(vals[i] + vals[i + 1] + vals[i + 2])
+
+current_depth = window_depths.pop(0)
 num_descents = 0
 
-for x in vals:
+for x in window_depths:
     if x > current_depth:
         num_descents += 1
     current_depth = x
-    
+
 print(num_descents)
